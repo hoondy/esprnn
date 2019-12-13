f81c44583da4f97f08e052043001bba3c647c91e
Donghoon Lee <dhrlee@gmail.com>
2019-12-13 14:42:58 -0500

    add span as parameter

--- preproc_bw2npy.py
+++ preproc_bw2npy.py
@@ -18,6 +18,7 @@ import pyBigWig
 parser = argparse.ArgumentParser(description='BW to NPY')
 parser.add_argument('-i','--bigwig', help='input bigWig file',required=True)
 parser.add_argument('-b','--bed',help='bed file', required=True)
+parser.add_argument('-s','--span',help='span', required=False, type=int, default=200)
 parser.add_argument('-p','--prefix',help='output prefix', required=True)
 args = parser.parse_args()
 
@@ -28,21 +29,20 @@ def bw2npy(bw_file, bed_file, npy_file):
 
     bw = pyBigWig.open(bw_file)
     bed=pd.read_csv(bed_file,sep='\t',header=None)
-    span=int(bed.loc[0,2])-int(bed.loc[0,1])
 
-    X = np.zeros((bed.shape[0], span, 1), dtype=np.float_) # set array of zeros with dim: n_sample, exon span, 1
+    X = np.zeros((bed.shape[0], 2*args.span, 1), dtype=np.float_) # set array of zeros with dim: n_sample, exon span, 1
 
     for i in range(bed.shape[0]):
         # if positive strand, store sigmoid normalized signal value
         if bed.loc[i,5]=='+':
-            tmp=np.array([sigmoid(x) for x in (bw.values(bed.loc[i,0], int(bed.loc[i,1]), int(bed.loc[i,2])))])
+            tmp=np.array([sigmoid(x) for x in (bw.values(bed.loc[i,0], int(bed.loc[i,1]-args.span), int(bed.loc[i,2]+args.span)))])
             np.nan_to_num(tmp)
             np.around(tmp, decimals=2)
             X[i,:,0]=tmp
 
         # if negative strand, reverse signal value
         else:
-            tmp=np.array([sigmoid(x) for x in (bw.values(bed.loc[i,0], int(bed.loc[i,1]), int(bed.loc[i,2])))][::-1])
+            tmp=np.array([sigmoid(x) for x in (bw.values(bed.loc[i,0], int(bed.loc[i,1]-args.span), int(bed.loc[i,2]+args.span)))][::-1])
             np.nan_to_num(tmp)
             np.around(tmp, decimals=2)
             X[i,:,0]=tmp
